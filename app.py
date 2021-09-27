a010ec981308f2bc0e43693db434a20ffd75bce4
Shawn Benny <sbenn9210@gmail.com>
2021-09-27 09:03:31 -0500

    Initial project setup

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Api
+from db import db 
+from dotenv import load_dotenv, find_dotenv
+import os
+
+
+load_dotenv(find_dotenv())
+
+from resources.user import UserRegister
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'shawn'
+api = Api(app)
+
+
+
+# create all the tables before the first request
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)  # important to mention debug=True
