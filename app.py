4f588a9a7b17d67e74c43e6e2c3e7325a480d94c
Shawn Benny <sbenn9210@gmail.com>
2021-09-29 10:56:55 -0500

    Add jwt for login

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask_restful import Api
 from db import db 
 from dotenv import load_dotenv, find_dotenv
+from flask_jwt import JWT
+from security import authenticate, identity
+from resources.user import UserRegister
 import os
 
-
 load_dotenv(find_dotenv())
-
-from resources.user import UserRegister, UserLogin
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'shawn'
+app.secret_key = os.getenv('SECRET_KEY')
 api = Api(app)
 
 
@@ -23,10 +23,17 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity)
+
+@jwt.jwt_error_handler
+def customized_error_handler(error):
+    return "The username or password is incorrect", 401
+
 api.add_resource(UserRegister, '/register')
-api.add_resource(UserLogin, '/login')
 
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(port=5000, debug=True)  # important to mention debug=True
+    app.run(port=5000, debug=True)
+
