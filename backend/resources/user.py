e6409c268037b0884cc842e017d098f77cca9c5b
Shawn Benny <sbenn9210@gmail.com>
2021-10-01 04:58:29 -0500

    Add jwt extended

--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token
 from models.user import UserModel
 import bcrypt
 
@@ -19,14 +20,42 @@ class UserRegister(Resource):
 
     def post(self):
         data = UserRegister.parser.parse_args()
-
-        if UserModel.find_by_username(data['username']):
+        username = data['username']
+        if UserModel.find_by_username(username):
             return {"message": "User with that username already exists."}, 400
         
         hashed = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
 
-        user = UserModel(data['username'], hashed)
+        user = UserModel(username, hashed)
         # user = UserModel(**data)
         user.save_to_db()
+        access_token = create_access_token(identity=username)
 
-        return {"message": "User created successfully."}, 201
+        return {"access_token": access_token}, 201
+
+class UserLogin(Resource):
+    TABLE_NAME = 'user'
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        username = data['username']
+        password = data['password']
+        user = UserModel.find_by_username(username)  
+        if user is None:
+            return {"message": "This user does not exist"}, 400
+
+        elif user and bcrypt.checkpw(password.encode(), user.password_hash):
+            access_token = create_access_token(identity=username)
+            return {"access_token": access_token}, 201
